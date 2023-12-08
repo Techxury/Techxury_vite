73f52c9d04c1848630c97a92a3dacb4bae9ca6de
Techxury <techxury@gmail.com>
2023-12-08 13:21:49 +0530

    update

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -14,7 +14,7 @@ const Main = () => {
                             <img className='mx-auto' src="./img/con1.webp" width={500} alt="" srcset="" />
                         </div>
 
-                        <div className=" pt-12 lg:text-left md:text-center sm:text-center">
+                        <div className=" pt-12 text-center">
                             <h2 className='main_con_title'>Unleash the Power of
                                 Collaboration</h2>
                             <p className='main_con_para'>Elevate your learning experience with our innovative collaboration feature. Connect seamlessly with peers, exchange ideas effortlessly. Our platform fosters a supportive learning community, allowing you to engage in group, share insights, and enhance your skills collectively. Say goodbye to learning alone—embrace the power of collaboration on your educational journey</p>
@@ -26,7 +26,7 @@ const Main = () => {
                     <div className="lg:columns-2  sm:columns-1" >
 
 
-                        <div className="  pt-12 lg:text-center md:text-center sm:text-center">
+                        <div className="  pt-12 text-center">
                             <h2 className='main_con_title'>Multi Level
                                 Doubt Solution</h2>
                             <p className='main_con_para '>"Navigate learning hurdles effortlessly with our Multi-Level Doubt Solution. From instant quiz assistance to group discussions and direct queries to course creators, we've got you covered at every step. No more struggling alone—get clear, concise answers in a variety of ways. Embrace the simplicity of our doubt solution feature, making your learning journey smoother and more enjoyable."</p>                        </div>
@@ -46,7 +46,7 @@ const Main = () => {
                             <img className='mx-auto' src="./img/con3.webp" width={500} alt="" srcset="" />
                         </div>
 
-                        <div className="   pt-12 lg:text-left md:text-center sm:text-center">
+                        <div className="   pt-12 text-center">
                             <h2 className='main_con_title'>Join Us
 Today !</h2>
                             <p className='main_con_para'>"Embark on a transformative learning journey with TECHXURY today. Elevate your skills, collaborate with peers, and unlock a world of opportunities. Join our dynamic community to access cutting-edge courses, engage in collaborative projects, and empower yourself for success in the ever-evolving tech landscape. Your future begins with TECHXURY."</p>
