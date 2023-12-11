a42039b630386b850c4491cdb38e06c1288d8da1
Techxury <techxury@gmail.com>
2023-12-11 13:55:16 +0530

    update nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,63 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { IoIosArrowDown } from "react-icons/io";
+import { IoReorderThree } from "react-icons/io5";
+
 
 const Header = () => {
-  // const [ProfileDisplay, setProfileDisplay] = useState(false)
+  const [ProfileDisplay, setProfileDisplay] = useState(false)
+  const [ScreenWidth, setScreenWidth] = useState(window.innerWidth)
+  const navbar_showing = () => {
+    setProfileDisplay(!ProfileDisplay)
 
-  // const pr_dis = () => {
-  //   setProfileDisplay(true)
+  }
 
-  // }
+  useEffect(() => {
+    const changeWidth = ()=>{
+      setScreenWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', changeWidth)
+  
+    return () => {
+      window.removeEventListener('resize', changeWidth)
+    }
+  }, [])
+  
   return (
     <>
-      <nav className='flex justify-around bg-purple-700 py-2 text-white'>
-        <div className='flex place-items-center'><Link className='text-2xl flex'><img src="img/final.webp" width={30} height={30} alt="" srcset="" className='mr-2' />Techxury</Link></div>
+    {(ProfileDisplay || ScreenWidth > 1023) && (<div className='hd_back_blur'></div>
+    )}
+      <nav className='flex lg:justify-around sm:justify-between bg-purple-700 py-2 text-white'>
+        <div className='flex place-items-center lg:ml-0 sm:ml-3'><Link className='text-2xl flex'><img src="img/final.webp" width={30} height={30} alt="" srcset="" className='mr-2' />Techxury</Link></div>
         <div>
-          <ul className='flex gap-5 my-auto place-items-center'>
+          {(ProfileDisplay || ScreenWidth > 1023) && (
+            <ul className=' gap-5 my-auto' id='head_links'>
             <li><NavLink to='/'>Home</NavLink></li>
             <li><NavLink to='/courses'>Courses</NavLink></li>
             <li><NavLink>About</NavLink></li>
-            <li><NavLink to='/profile'>profile</NavLink></li>
-            {/* <li className='mt-1'><button className=''><NavLink to='/login' className='text-white bg-orange-500 p-3 rounded-lg'>Login</NavLink></button></li> */}
-            <li className='flex place-items-center'><img src="img/final.webp" width={30} height={30} className=' rounded-full mr-1' alt="" srcset="" />
+            <li><NavLink to='/profile'>Profile</NavLink></li>
+            <li className='mt-1'><button className=''><NavLink to='/login' className='text-white bg-orange-500 p-3 rounded-lg'>Login</NavLink></button></li>
+            <li className='flex place-items-center'>
+            <img src="img/final.webp" width={30} height={30} className=' rounded-full mr-1' alt="" srcset="" />
               <h5>Error 404</h5>
-              <span id='login_arrow' className='ml-1 cursor-pointer text-xl' ><IoIosArrowDown /></span>
+              <span   id='login_arrow' className='ml-1 cursor-pointer text-xl' ><IoIosArrowDown /></span>
+              {/* <ul className='flex flex-col absolute top-12 right-20 bg-slate-500 p-2'>
+                <li><NavLink to='/profile'>My Profie</NavLink></li>
+                <li><NavLink>Logout</NavLink></li>
+              </ul> */}
             </li>
-            
-            <div className='header_ac_profile relative p-3 text-white'>
+
+              {/* dropdown menu peofile section  */}
+            {/* <div className=' p-3 text-white'>
               <ul className='flex flex-col'>
                 <li><NavLink to='/profile'>My Profie</NavLink></li>
-
                 <li><NavLink>Logout</NavLink></li>
               </ul>
-            </div>
-          </ul>
+            </div> */}
 
+          </ul>)}
+
+          <button onClick={navbar_showing} className='nav_btn_mobile'><IoReorderThree /></button>
         </div>
       </nav>
     </>
