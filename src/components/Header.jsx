d031e3a9cb20e6f9cbbb30dbc0964a000af2075b
Techxury <techxury@gmail.com>
2023-12-13 22:54:30 +0530

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,12 @@ const Header = () => {
   const [ProfileDisplay, setProfileDisplay] = useState(false)
   const [Profile_setting, setProfile_setting] = useState(false)
   const [ScreenWidth, setScreenWidth] = useState(window.innerWidth)
+
   const navbar_showing = () => {
     setProfileDisplay(!ProfileDisplay)
 
   }
-  const profile_showing = ()=>{
+  const profile_showing = () => {
     setProfile_setting(!Profile_setting)
   }
 
@@ -36,25 +37,23 @@ const Header = () => {
         <div>
           {(ProfileDisplay || ScreenWidth > 1023) && (
             <ul className=' gap-5 my-auto' id='head_links'>
-              <li><NavLink to='/'>Home</NavLink></li>
-              <li><NavLink to='/courses'>Courses</NavLink></li>
-              <li><NavLink>About</NavLink></li>
-              <li><NavLink to='/profile'>Profile</NavLink></li>
+              <li className='mt-1'><NavLink to='/' className={({isActive})=> `px-2 py-2 font-semibold ${isActive ? 'text-black bg-white' : 'text-white bg-none'} rounded-lg`}>Home</NavLink></li>
+              <li className='mt-1'><NavLink to='/courses' className={({isActive})=> `px-2 py-2 font-semibold ${isActive ? 'text-black bg-white' : 'text-white bg-none'} rounded-lg`}>Courses</NavLink></li>
+              <li className='mt-1'><NavLink to='/about' className={({isActive})=> `px-2 py-2 font-semibold ${isActive ? 'text-black bg-white' : 'text-white bg-none'} rounded-lg`}>About</NavLink></li>
+              <li className='mt-1'><NavLink to='/profile' className={({isActive})=> `px-2 py-2 font-semibold ${isActive ? 'text-black bg-white' : 'text-white bg-none'} rounded-lg`}>Profile</NavLink></li>
               <li className='mt-1'><button className=''><NavLink to='/login' className='text-white bg-orange-500 p-3 rounded-lg'>Login</NavLink></button></li>
 
               <div>
-                <li className='flex place-items-center'  onClick={profile_showing}>
+                <li className='flex place-items-center my-auto' onClick={profile_showing}>
 
                   <img src="img/final.webp" width={30} height={30} className=' rounded-full mr-1' alt="" srcset="" />
                   <h5>Error 404</h5>
                   <span id='login_arrow' className='ml-1 cursor-pointer text-xl mt-1' ><IoIosArrowDown /></span>
-
-
                 </li>
-                {Profile_setting  && (<li><div className='lg:border border-black px-3 lg:text-black mt-2 lg:bg-gray-200 sm:text-white font-medium' >
+                {Profile_setting && (<li><div className='lg:border border-black px-3 lg:text-black mt-2 lg:bg-gray-200 sm:text-white font-medium' >
                   <ul className='flex flex-col'>
-                    <li><NavLink to='/profile'>My Profie</NavLink></li>
-                    <li><NavLink>Logout</NavLink></li>
+                    <li><NavLink to='/profile' className={({isActive})=> `px-2 py-2 font-semibold ${isActive ? 'text-orange-500 ' : 'text-black bg-none'} rounded-lg`}>My Profie</NavLink></li>
+                    <li><NavLink className='px-2 py-2 font-semibold'>Logout</NavLink></li>
                   </ul>
                 </div></li>)}
               </div>
